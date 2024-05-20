712b9c43890328f5999da1f7a9f1fc6556ef1348
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-05-21 02:21:18 +0500

    add: Error: Invalid schema configuration: `onboarded` schematype definition is invalid.
    can't get past this "Error: Invalid schema configuration: `onboarded` schematype definition is invalid.". 😪

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware(
+const isProtectedRoute = createRouteMatcher([
     {
-        publicRoutes: ['/', '/api/webhook/clerk'],
-        ignoredRoutes: ['/api/webhook/clerk']
+        publicRoutes: ['/', '/pages/api/webhooks/clerk'],
+        ignoredRoutes: ['/pages/api/webhooks/clerk']
     }
-);
+]);
+
+export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
-    // The following matcher runs middleware on all routes
-    // except static assets.
     matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
